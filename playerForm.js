4ec6ec8ff0b97b202909c42774a7c645bbddd677
LottaSvanhall <charlotte.svanhall@live.se>
2023-11-15 12:14:16 +0100

    Update playerForm.js
    With errorhandling. Still issue with empty element 0 in array.

--- playerForm.js
+++ playerForm.js
@@ -1,3 +1,5 @@
+document.querySelector('#button').addEventListener('click', handleLogin);
+
 const players = [ //array med objekt i
   {
     username: '',
@@ -5,38 +7,49 @@ const players = [ //array med objekt i
   }
 ];
 
-
-document.querySelector('#button').addEventListener('click', handleLogin);
-//id för form, lyssnare av formen submit, triggar funktion nedan
 JSON.parse(localStorage.getItem("players"))
 console.log(players)
+
 
 function handleLogin(event) { //tar in event (e)
   event.preventDefault(); //för att ta bort originalsettings för form och använda min egen vid submit av formulär
   //alert("Du har submittat formuläret!!")
 
   const username = document.getElementById('username').value;
-  console.log("Username - ", username);
+  //const score = document.getElementById(score).value;
 
-  players.push(username);
-  
+  const userExists = players.some(players =>  //går igenom users listan och kollar om det liknar något vi fått in innan
+    players.username.toLowerCase() === username.toLowerCase()); //om match så blir boolean satt till true annars false 
+  // console.log("Did user exist - ", userExists);
+
+  if (!userExists) {
+
+    alert('Användaren skapad!');
+    players.push({username, score:0}); //HUR GÖRA MED SCORE!!!!!!!
+
+  }
+  else {           //om ingen match
+    alert('Användarnamnet finns redan!');
+  }
+
+  //console.log("Username - ", username);
+
 
   localStorage.setItem("Players", JSON.stringify(players));
 
-  const userExists = players.some(players =>  //går igenom users listan och kollar om det liknar något vi fått in innan
-    players.username === username); //om match så blir boolean satt till true annars false 
-  console.log("Did user exist - ", userExists);
-
-/*
-
-  if (userExists) {  //om match
-  window.location.href = './player.html';
-    alert('Du har skapat användarnamnet!');
-  
-  } else {           //om ingen match
-  alert('Användarnamnet finns redan!');
-  }
-*/
- 
-
+  //handleLogin()
 }
+
+
+/* const userExists = players.some(players =>  //går igenom users listan och kollar om det liknar något vi fått in innan
+   players.username.toLowerCase() === username.toLowerCase()); //om match så blir boolean satt till true annars false
+ // console.log("Did user exist - ", userExists);
+
+ if (!userExists) {  //om match
+   alert('Du har redan skapat användarnamnet!');
+
+ } else {           //om ingen match
+   alert('Användarnamnet finns redan!');
+ }*/
+
+// handleLogin()
