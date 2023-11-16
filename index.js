0ff01a2ec82585a51b4132d551cdbd3ddf97bb0b
LottaSvanhall <charlotte.svanhall@live.se>
2023-11-16 17:02:09 +0100

    create and choose users fixed_local storage
    Äntligen funkar det att skapa användare och fånga upp dem på spelsidan i select menyn, pust.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const gameBoard = document.querySelector("#gameboard")
 const infoDisplay = document.querySelector("#info")
-const playerForm = document.querySelector("#playerForm")
+const playerMenu = document.querySelector("#playerMenu")
+
 
 const startCells = [
   "", "", "", "", "", "", "", "", ""
 ] //för att skapa nio celler
+
 
 let go = 'circle'
 infoDisplay.textContent = "Cirkel börjar!"
@@ -29,7 +31,7 @@ function addGo(event) {
     event.currentTarget.append(goDisplay) //placerar cirkel eller line i rutan vi klickat på
     go = go === 'circle' ? 'line' : 'circle' //bestämmer vilken som är sann och vilkens tur det är
     infoDisplay.textContent = "Det är nu " + go + "'s tur att lägga."
-    
+
     checkScore() //kollar vinnande kombinationer vid varje drag
 
   }
@@ -38,18 +40,18 @@ function addGo(event) {
     event.currentTarget.firstChild.remove()
   }
 }
-  //event.target.removeEventListener('click', addGo) //tar bort eventlistener och klick och den andres tur
-  
+//event.target.removeEventListener('click', addGo) //tar bort eventlistener och klick och den andres tur
+
 
 function isAllowedToPlace() {
   let count = 1;
   const allSquares = document.querySelectorAll('.square')
-  for (i = 0; i < allSquares.length; i++){
+  for (i = 0; i < allSquares.length; i++) {
     if (allSquares[i].firstChild?.classList.contains(go)) {
       count += 1;
     }
   }
-  return count<=3
+  return count <= 3
 }
 
 function checkScore() {
@@ -62,7 +64,7 @@ function checkScore() {
   ]
 
   winningCombos.forEach(array => { //för varje array av de nio ovan
-   const circleWins = array.every(cell => //för varje cell i varje array
+    const circleWins = array.every(cell => //för varje cell i varje array
       allSquares[cell].firstChild?.classList.contains('circle')) //kollar av om cirkeln har en vinnande kombination
 
     if (circleWins) {
@@ -82,39 +84,56 @@ function checkScore() {
       return
     }
   })
+
 }
 
-const players = ['', '', '', '', '']; //lista med djur
 
-const form = document.createElement('form');
-form.setAttribute('id', 'playerForm'); //id ska vara playerform går också att skriva form.id = 'playerForm';
 
-const select = document.createElement('select'); //skapar selectelement
-select.setAttribute('id', 'playerSelect'); //sätter id som är playerSelect
-select.setAttribute('name', 'players'); //sätter namn som är player, ej nödvändigt att sätta detta
-
-users.forEach(players => {
-  const option = document.createElement('option'); //loop för att skapa alla options
-  option.value = players.toLowerCase();
-  option.textContent = players; //skriver ut Hund med stor bokstav
-  select.appendChild(option); //lägger till som en option
-});
-
-const submitBtn = document.createElement('input');
-submitBtn.setAttribute('type', 'submit');
-submitBtn.value = 'Bekräfta val'; //hund med litet h kommer ifrån value
-
-form.appendChild(select);
-form.appendChild(submitBtn);
-
-document.body.appendChild(form);
-
-form.addEventListener('submit', function (event) { //eventlistner för submit
-  event.preventDefault();
-  console.dir(select)
-  console.log(select.selectedIndex);
-  const valdSpelare = select.options[select.selectedIndex].value; //vilken option index i listan är det som är aktiv och vald
-  alert('Du har valt: ' + valdSpelare);  //vi vill inte göra alert utan andra saker
-});
-
+function choosePlayers() {
   
+  const tempPlayers = localStorage.getItem("players")
+  const players = tempPlayers ? JSON.parse(tempPlayers) : [];
+  //const players = JSON.parse(localStorage.getItem("players")); //hämta lista med spelare
+  //const label1 = document.createElement('label')
+
+  const form1 = document.createElement('form');
+  const select1 = document.createElement('select'); //skapar selectelement
+
+  form1.setAttribute('id', 'playerDrop1'); //id ska vara playerform går också att skriva form.id = 'playerForm';
+  select1.setAttribute('id', 'playerSelect1'); //sätter id som är playerSelect
+
+  //select.setAttribute('name', 'players'); //sätter namn som är player, ej nödvändigt att sätta detta
+
+  players.forEach(player => { //NÅGOT FEL HÄR
+    const option = document.createElement('option'); //loop för att skapa alla options
+    option.value = player.Name; 
+    option.textContent = player.Name; //skriver ut Hund med stor bokstav
+    select1.appendChild(option); //lägger till som en option
+  });
+
+  const submitBtn = document.createElement('input');
+  submitBtn.setAttribute('type', 'submit');
+  submitBtn.value = 'Bekräfta val'; //hund med litet h kommer ifrån value
+
+  //form1.appendChild(label1);
+  //label1.innerText = "Cirkel: "
+
+  form1.appendChild(select1);
+  form1.appendChild(submitBtn);
+console.log(playerMenu)
+  //document.body.appendChild(form1)
+ playerMenu.appendChild(form1); //NÅGOT FEL HÄR
+
+  form1.addEventListener('submit', function (event) { //eventlistner för submit
+    event.preventDefault();
+    console.dir(select1)
+    console.log(select1.selectedIndex);
+   // circleName = select1.options[select1.selectedIndex].value; //vilken option index i listan är det som är aktiv och vald
+    //alert('Du har valt: ' + valdSpelare);  //vi vill inte göra alert utan andra saker
+  });
+
+}
+
+choosePlayers()
+
+
--- playerForm.js
+++ playerForm.js
@@ -1,55 +1,42 @@
 document.querySelector('#button').addEventListener('click', handleLogin);
 
-const players = [ //array med objekt i
-  {
-    username: '',
-    score: 0,
-  }
-];
-
-JSON.parse(localStorage.getItem("players"))
-console.log(players)
+const tempPlayers = localStorage.getItem("players")
+const players = tempPlayers ? JSON.parse(tempPlayers) : [];
+//console.log(players)
 
 
-function handleLogin(event) { //tar in event (e)
-  event.preventDefault(); //för att ta bort originalsettings för form och använda min egen vid submit av formulär
+function handleLogin() { //tar in event (e)
+ //event.preventDefault(); //för att ta bort originalsettings för form och använda min egen vid submit av formulär
   //alert("Du har submittat formuläret!!")
+ /* const player =  //array med objekt i
+    {
+      username: '',
+      score: 0,
+    }
+*/
 
   const username = document.getElementById('username').value;
   //const score = document.getElementById(score).value;
 
   const userExists = players.some(players =>  //går igenom users listan och kollar om det liknar något vi fått in innan
-    players.username.toLowerCase() === username.toLowerCase()); //om match så blir boolean satt till true annars false 
-  // console.log("Did user exist - ", userExists);
+    players.Name.toLowerCase() === username.toLowerCase()); //om match så blir boolean satt till true annars false 
+  
 
   if (!userExists) {
-
+    const player = 
+    {
+      Name: username,
+      Score: 0,
+    }
     alert('Användaren skapad!');
-    players.push({username, score:0}); //HUR GÖRA MED SCORE!!!!!!!
-
+    players.push(player);
   }
-  else {           //om ingen match
+
+  else {           
     alert('Användarnamnet finns redan!');
   }
 
-  //console.log("Username - ", username);
+  localStorage.setItem("players", JSON.stringify(players));
 
-
-  localStorage.setItem("Players", JSON.stringify(players));
-
-  //handleLogin()
+  
 }
-
-
-/* const userExists = players.some(players =>  //går igenom users listan och kollar om det liknar något vi fått in innan
-   players.username.toLowerCase() === username.toLowerCase()); //om match så blir boolean satt till true annars false
- // console.log("Did user exist - ", userExists);
-
- if (!userExists) {  //om match
-   alert('Du har redan skapat användarnamnet!');
-
- } else {           //om ingen match
-   alert('Användarnamnet finns redan!');
- }*/
-
-// handleLogin()
